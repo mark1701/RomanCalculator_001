3a717d63fbc2558d4c0d8ea7a6f99f071bf39e9a
marco <none@none.com>
2018-12-06 23:23:41 +0000

    added tests

--- RomanCalculatorTests/ProgramTests.cs
+++ RomanCalculatorTests/ProgramTests.cs
@@ -5,11 +5,36 @@ namespace RomanCalculator.Tests
     class ProgramTests
     {
         [Test]
-        public void Stub()
+        public void Adding_two_empty_numbers_returns_an_empty_sum()
         {
+            var firstNumber = "";
+            var secondNumber = "";
 
+            var actualSum = firstNumber + secondNumber;
 
-            Assert.Equals(1, 2);
+            Assert.AreEqual("", actualSum);
         }
+
+        [Test]
+        public void Adding_an_empty_number_to_a_non_empty_number_returns_the_non_empty_number() {
+            var nonEmptyNumber = "I";
+            var emptyNumber = "";
+
+            var actualSum = nonEmptyNumber + emptyNumber;
+
+            Assert.AreEqual(nonEmptyNumber, actualSum);
+        }
+
+        [Test]
+        public void Adding_a_non_empty_number_to_an_empty_number_returns_the_non_empty_number()
+        {
+            var emptyNumber = "";
+            var nonEmptyNumber = "I";
+
+            var actualSum = emptyNumber + nonEmptyNumber;
+
+            Assert.AreEqual(nonEmptyNumber, actualSum);
+        }
+
     }
 }
